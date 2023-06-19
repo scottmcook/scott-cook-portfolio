77685ad11e9cd41c931f27ab62c3e133a1a1cd00
scottmcook <scott.madison.cook@gmail.com>
2023-06-18 21:49:55 -0700

    Remove medium

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,8 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
+        <InterestsSection sectionId="skills" heading="Skills" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
         <ContactSection sectionId="github" heading="Issues?" />
       </Page>
